9cc574e00a35d9cf84fd4040afa46bccf5630108
Zhao Jiangkun <zhaojk90@gmail.com>
2017-01-06 16:45:29 +0000

    compare the triplets

---
+++ hackerrank/algorithms/compare_the_triplets.hs
@@ -0,0 +1,30 @@
+import Control.Applicative
+import Control.Monad
+import System.IO
+
+main :: IO()
+main = do
+     a0_temp <- getLine
+     let a0_t = words a0_temp
+     let a0 = read $ a0_t!!0 ::Int
+     let a1 = read $ a0_t!!1 :: Int
+     let a2 = read $ a0_t!!2 :: Int
+     b0_temp <- getLine
+     let b0_t = words b0_temp
+     let b0 = read $ b0_t!!0 :: Int
+     let b1 = read $ b0_t!!1 :: Int
+     let b2 = read $ b0_t!!2 :: Int
+
+     let res = calc_score [a0,a1,a2] [b0,b1,b2]
+
+     print res
+     
+calc_score :: [Int] ->  [Int] -> (Int,Int)
+
+calc_score [] _ = (0,0)
+calc_score _ [] = (0,0)
+calc_score (a:as) (b:bs) = 
+           if a>b then (reta+1,retb) else
+           if a<b then (reta,retb+1)
+           else (reta,retb)
+           where (reta,retb) = calc_score as bs
